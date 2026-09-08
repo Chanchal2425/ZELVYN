438985d95529b8170fa6d72f2e9477d9dfd1c47f
chanchal <chanchalmankar11@gmail.com>
2026-09-08 17:02:57 +0530

    Updated vercel.json file

--- server.js
+++ server.js
@@ -16,7 +16,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve your website files
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname), {
+  index: "index.html"
+}));
 
 // --------------------------------------------------
 // Gmail transporter
